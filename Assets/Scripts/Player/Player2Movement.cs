e6f40da8e9fe810b5c20187834616d8a648b0f96
Alex-Senst <asenst2004@gmail.com>
2026-02-28 21:30:43 -0800

    Attempt at Player Movement

---
+++ Assets/Scripts/Player/Player2Movement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player2Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float turnSpeed = 180f;
+
+    private Rigidbody rb;
+    private InputAction moveAction;
+
+    private float yaw = 90f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        moveAction = InputSystem.actions.FindAction("Move2");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+
+        float forwardInput = input.y;
+        float turnInput = input.x;
+
+        yaw += turnInput * turnSpeed * Time.fixedDeltaTime;
+        rb.MoveRotation(Quaternion.Euler(0f, yaw, 0f));
+
+        Vector3 forward = transform.forward * forwardInput * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forward);
+
+        rb.angularVelocity = Vector3.zero;
+    }
+}
